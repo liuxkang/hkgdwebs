033c8b0fbc74930c32be3edbb4a0466e934270cf
liuxkang <liuxkang@hotmail.com>
2016-06-03 00:17:11 +0800

    201606033学习页面显示数据

--- hkgdwebs/urls.py
+++ hkgdwebs/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 urlpatterns = [
-    url(r'^$',include("ipdb.urls"))
+    url(r'^$',include("ipdb.urls")),
+	url(r'^admin/',admin.site.urls),
 ]
--- ipdb/views.py
+++ ipdb/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Ipdb
 # Create your views here.
 def index(request):
-    return render(request,'ipdb/index.html')
+    datatmp = Ipdb.objects.all()
+    return render(request,'ipdb/index.html',datatmp)
 
