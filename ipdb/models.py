c336119d0403c484221000b07646485eb2d01dd9
liuxkang <liuxkang@hotmail.com>
2016-07-02 14:11:39 +0800

    2016年7月2日修改

--- ipdb/models.py
+++ ipdb/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Ipdb(models.Model):
-    mac_addr = models.CharField(max_length=14)
-    ip_addr = models.CharField(max_length=14)
-    vlan_id = models.CharField(max_length=5)
-    dept = models.CharField(max_length=20)
-    noted = models.CharField(max_length=100)
+    ip_addr = models.GenericIPAddressField(primary_key = True)
+    mac_addr = models.CharField(max_length=14,null=True)  
+    vlan_id = models.CharField(max_length=10)
+    dept = models.CharField(max_length=20,null=True)
+    noted = models.CharField(max_length=100,null=True)
     def __str__(self):
         return self.mac_addr
 
--- web_nav/models.py
+++ web_nav/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Web_nav(models.Model):
     name = models.CharField(max_length=30)
     addr = models.CharField(max_length=255)
+    noted = models.TextField(null=True)
     img_name = models.CharField(max_length=100,null=True)
     def __str__(self):
         return self.name
