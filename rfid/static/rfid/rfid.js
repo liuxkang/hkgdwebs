9fb22ac29efca22da323913ab8b42ccd29094af6
liuxkang <liuxkang@hotmail.com>
2016-07-07 23:28:37 +0800

    初步完成rfid模块

--- rfid/static/rfid/rfid.js
+++ rfid/static/rfid/rfid.js
@@ -1,21 +1,16 @@
 $(document).ready(initAll);
 
 var rfid_value;
-var it_type_value;
-var sn_value;
-//var dept_value;
-var user_value;
-var time_value;
 
 function initAll()
 {
-	$('.edit_button').click(edit_mode);				//设置“修改”按钮的点击事件
-	$('.no_button').click(watch_mode);				//设置“取消”按钮的点击事件
+	$('button[value="edit_button"]').click(edit_mode);				//设置“修改”按钮的点击事件
+	$('button[value="no_button"]').click(watch_mode);				//设置“取消”按钮的点击事件
 	$('input').addClass("hide_mode");
 	$('select').addClass("hide_mode");
-	$('.edit_button').addClass("show_mode");
-	$('.ok_button').addClass("hide_mode");
-	$('.no_button').addClass("hide_mode");
+	$('button[value="edit_button"]').addClass("show_mode");
+	$('button[value="ok_button"]').addClass("hide_mode").click(Check_2_Submit);
+	$('button[value="no_button"]').addClass("hide_mode");
 	$('.page_a').click(submit);
 }	
 
@@ -25,9 +20,9 @@ function edit_mode()			//编辑模式
 {
 	$('input').removeClass().addClass("hide_mode");
 	$('select').removeClass().addClass("hide_mode");
-	$('.edit_button').removeClass().addClass("hide_mode");
-	$('.ok_button').removeClass().addClass("hide_mode");
-	$('.no_button').removeClass().addClass("hide_mode");
+	$('button[value="edit_button"]').removeClass().addClass("hide_mode");
+	$('button[value="ok_button"]').removeClass().addClass("hide_mode");
+	$('button[value="no_button"]').removeClass().addClass("hide_mode");
 	
 	$('div[id="th_type_'+this.id+'"]').removeClass().addClass("hide_mode");
 	$('div[id="th_sn_'+this.id+'"]').removeClass().addClass("hide_mode");
@@ -52,31 +47,24 @@ function edit_mode()			//编辑模式
 	$('select[id="type_'+this.id+'"]').val(type_value);
 	
 	var time_value = $('div[id="th_time_'+this.id+'"]').text();
+	time_value = time_value.replace("年","-");
+	time_value = time_value.replace("月","-");
+	time_value = time_value.replace("日","");
 	$('input[id="time_'+this.id+'"]').val(time_value);
+	
+	rfid_value = $('button[value="edit_button"]').get(0).id;
 	return true;
 }
 
 function watch_mode()				//查看模式
 {
-	/*
-	$('.edit_button').css("display","inline");
-	$('.delete_button').css("display","inline");
-	$('.ok_button').css("display","none");
-	$('.no_button').css("display","none");
-	var mac_edit_input = document.getElementById("mac_"+this.name);
-	var noted_edit_input = document.getElementById("noted_"+this.name);
-	mac_edit_input.setAttribute("type","hidden");
-	noted_edit_input.setAttribute("type","hidden");
+	$('button[value="edit_button"]').removeClass().addClass("show_mode");
+	$('button[value="ok_button"]').removeClass().addClass("hide_mode");
+	$('button[value="no_button"]').removeClass().addClass("hide_mode");
+	$('input').removeClass().addClass("hide_mode");
+	$('select').removeClass().addClass("hide_mode");
+	$('div').removeClass().addClass("show_mode");
 	
-	document.getElementById("dept_"+this.name).style.display = "none";
-	document.getElementById("th_dept_"+this.name).innerHTML = dept_value;
-	document.getElementById("th_noted_"+this.name).innertHTML = noted_value;
-	document.getElementById("th_mac_"+this.name).innerHTML = mac_value;
-	document.getElementById("th_dept_"+this.name).style.display = "inline";
-	document.getElementById("dept_"+this.name).style.display = "none";
-	document.getElementById("th_noted_"+this.name).style.display = "inline";
-	document.getElementById("th_mac_"+this.name).style.display = "inline";
-	*/
 	return true;
 }
 
@@ -85,4 +73,24 @@ function submit()
 	var page = this.id;
 	$('#page_form').append('<input type="hidden" name="page" value="'+page+'" />');
 	$('#page_form').submit();
+}
+
+function Check_2_Submit()
+{
+	var time_value = $('input[id="time_'+rfid_value+'"]').val();
+	var re = /^(\d{4})-(\d{1,2})-(\d{1,2})$/;
+	var result = re.test(time_value);
+	if(result == true){
+		if(RegExp.$1 > 2020 || RegExp.$1 < 1980 || RegExp.$2 > 12 || RegExp.$2 < 1 || RegExp.$3 > 31 || RegExp.$3 < 1)
+		{
+			$('input[id="time_'+rfid_value+'"]').addClass("invalid");
+			return false;
+		}
+		$('form[class="'+rfid_value+'"]').submit();
+		return true;
+	}
+	else{
+		$('input[id="time_'+rfid_value+'"]').addClass("invalid");
+		return false;
+	}
 }
