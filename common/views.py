b5e29759bdca00ca21238d90bb84c4bb0d8f6be6
liuxkang <liuxkang@hotmail.com>
2016-07-30 22:56:39 +0800

    增加登陆模块

--- common/views.py
+++ common/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from .models import Users,Depts
 # Create your views here.
+def user_signin(request):
+    users = Depts.objects.all()
+    return render_to_response("common/user_signin.html",locals())
